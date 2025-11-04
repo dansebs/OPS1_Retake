fc3f49f23b6b5ed318ad719410c29f51dc50b9ad
dansebs <danny_ruiz00@hotmail.com>
2025-11-04 11:17:14 +0100

    Stage 2

--- Lab1/Sample_Task1/sptask1.c
+++ Lab1/Sample_Task1/sptask1.c
@@ -5,13 +5,15 @@
 #include <sys/stat.h>
 
 #define ERR(source) (perror(source), fprintf(stderr, "%s:%d\n", __FILE__, __LINE__), exit(EXIT_FAILURE))
+#define MAX_PATH 101
+
 
 void scan_dir()
 {
     DIR *dirp;
     struct dirent *dp;
     struct stat filestat;
-    int dirs = 0, files = 0, links = 0, other = 0;
+    long int sum = 0;
     if ((dirp = opendir(".")) == NULL)
         ERR("opendir");
     do
@@ -19,19 +21,29 @@ void scan_dir()
         errno = 0;
         if ((dp = readdir(dirp)) != NULL)
         {
-            printf("%s\n",dp->d_name);
+            if ((dp = readdir(dirp)) != NULL)
+        {
+            if (lstat(dp->d_name, &filestat))
+                ERR("lstat");
+            
+            sum += filestat.st_size;
+            printf("%s %ld\n",dp->d_name,filestat.st_size);
+        }
         }
     } while (dp != NULL);
-
+    printf("%ld\n", sum);
     if (errno != 0)
         ERR("readdir");
     if (closedir(dirp))
         ERR("closedir");
-    printf("Files: %d, Dirs: %d, Links: %d, Other: %d\n", files, dirs, links, other);
+
 }
 
 int main(int argc, char **argv)
 {
+    char path[MAX_PATH];
+    if(getcwd(path, MAX_PATH) == NULL) ERR("getcwd");
+
     scan_dir();
     return EXIT_SUCCESS;
 }
