af67fffca2df4a6ce5bbd085d59b6ace3d6c909f
dansebs <danny_ruiz00@hotmail.com>
2026-01-19 15:40:15 +0100

    Lab4

---
+++ Lab4/Task1/task1_1.c
@@ -0,0 +1,10 @@
+#define _GNU_SOURCE
+#include <errno.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
